b237d5addc7d7396d9d2c7b871f303eec439d68d
FacundoMinetto <facundominetto@gmail.com>
2024-03-19 12:03:40 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <>
+      <div>
+        <h3>dsa</h3>
+      </div>
+      <h1 style={{ color: "peru", fontSize: "1rem", marginTop: "10px" }}>
+        Home
+      </h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "../styles/Navbar.css";
+export const Navbar = () => {
+  return (
+    <div className="container">
+      <h2>Navbar</h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+    </div>
+  );
+};
