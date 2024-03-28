f720f02d3d474c2cf820013701352621e487e0da
FacundoMinetto <facundominetto@gmail.com>
2024-03-28 11:45:24 -0300

    1er entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import { Navbar } from "./components/layout/navbar/Navbar";
-import { Footer } from "./components/layout/footer/Footer";
+import { Navbar } from "./components/navbar/Navbar";
 import { ItemListContainer } from "./components/pages/itemListContainer/itemListContainer";
 
-// name ---> pepe
-//name ---> juan
-
-const [name, setName] = useState("pepe");
-const cambiarNombre = () => {
-  setName("juan");
-};
 function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainer />
-      <Footer />
-
-      <button onclick={cambiarNombre}>Cambiar nombre</button>
+      <ItemListContainer greeting={"Hola a todos!"} />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import { cartWidget } from "../pages/cart/cartWidget";
+export const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            CLOTHESBrand
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Productos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contacto
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Guia de talles
+                </a>
+              </li>
+              <li>
+                <cartWidget />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
